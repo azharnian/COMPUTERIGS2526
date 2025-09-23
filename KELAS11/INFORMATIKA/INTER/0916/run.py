a090b68d8f5f157e9cf1e430e1d0432516d548b0
Anas Azhar <anas.sty@gmail.com>
2025-09-23 14:20:54 +0700

    23 09 2025

--- KELAS11/INFORMATIKA/INTER/0916/run.py
+++ KELAS11/INFORMATIKA/INTER/0916/run.py
@@ -1,19 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, g
 import sqlite3
+from datetime import datetime
 
 app = Flask(__name__)
 
 database = "./data.db"
 
 def get_db():
-    pass
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(database)
+    return db
 
 def close_db():
-    pass
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
 
-@app.route("/") #request end point via browser, via api, etc
+users = [
+    {
+        "first" : "Budi",
+        "last" : "Santoso"
+    },
+    {
+        "first" : "Cindy",
+        "last" : "Siregar"
+    }
+]
+
+@app.route("/", methods=["GET", "POST"]) 
 def index():
-    return render_template("index.html") #response yang dibuat oleh programmer
+    if request.method == "POST":
+        first = request.form.get("first")
+        last = request.form.get("last")
+
+        user = {"first" : first, "last" : last}
+        users.append(user)
+
+    return render_template("index.html", users=users) 
+
+@app.route("/isitnewyear")
+def isitnewyear():
+    today = datetime.now()
+
+    new_year = "NO"
+    if today.day == 1 and today.month == 1:
+        new_year = "YES"
+        
+    return render_template("newyear.html", new_year=new_year)
 
 
 
