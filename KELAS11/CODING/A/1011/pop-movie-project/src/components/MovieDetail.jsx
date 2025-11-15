478f8449963800ab0d7883ff2a83f32585a83d91
Anas Azhar <anas.sty@gmail.com>
2025-11-15 11:29:44 +0700

    15-11-2025

--- KELAS11/CODING/A/1011/pop-movie-project/src/components/MovieDetail.jsx
+++ KELAS11/CODING/A/1011/pop-movie-project/src/components/MovieDetail.jsx
@@ -1,38 +1,69 @@
-export default function MovieDetail({ handleSetNullSelectedMovie }){
-    return (
-        <>
-        <header>
-            <button className="btn-back"
-                    onClick={handleSetNullSelectedMovie}
-            >
-              &#x2715;
-            </button>
-            <img src={null} alt={`${null} poster`} />
-            <div className="details-overview">
-              <h2>{null}</h2>
-              <p>
-                <span>📅</span>
-                <span>{null}</span>
-              </p>
-              <p>
-                <span>⏳</span>
-                <span>{null}</span>
-              </p>
-              <p>
-                <span>🌟</span>
-                <span>{null}</span>
-              </p>
-            </div>
-          </header>
-          <section>
+import { useState, useEffect } from "react";
+import { fetchMovieDetail } from "../helpers";
+
+export default function MovieDetail({ handleSetNullSelectedMovie, selectedMovie }){
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+      async function fetchData() {
+        try {
+          const data = await fetchMovieDetail(selectedMovie);
+          setMovie(data);
+        } catch(err) {
+          console.error(err);
+        }
+      }
+
+      fetchData();
+  }, [selectedMovie]);
+
+  const {
+    Title: title,
+    Year: year,
+    Released: released,
+    Poster: poster,
+    imdbRating,
+    Runtime: runtime,
+    Plot: plot,
+    Genre: genre,
+    Actors: actors,
+    Director: director,
+  } = movie;
+
+  return (
+      <>
+      <header>
+          <button className="btn-back"
+                  onClick={handleSetNullSelectedMovie}
+          >
+            &#x2715;
+          </button>
+          <img src={poster} alt={`${title} poster`} />
+          <div className="details-overview">
+            <h2>{title}</h2>
             <p>
-              <em>{null}</em>
+              <span>📅</span>
+              <span>{released}</span>
             </p>
-            <p>Year: {null}</p>
-            <p>Genre: {null}</p>
-            <p>Starring: {null}</p>
-            <p>Directed by: {null}</p>
-          </section>
-        </>
-    )
+            <p>
+              <span>⏳</span>
+              <span>{runtime}</span>
+            </p>
+            <p>
+              <span>🌟</span>
+              <span>{imdbRating}</span>
+            </p>
+          </div>
+        </header>
+        <section>
+          <p>
+            <em>{plot}</em>
+          </p>
+          <p>Year: {year}</p>
+          <p>Genre: {genre}</p>
+          <p>Starring: {actors}</p>
+          <p>Directed by: {director}</p>
+        </section>
+      </>
+  )
 }
--- KELAS11/CODING/A/1011/pop-movie-project/src/helpers/index.js
+++ KELAS11/CODING/A/1011/pop-movie-project/src/helpers/index.js
@@ -6,10 +6,20 @@ export async function fetchMovie(query) {
             `http://www.omdbapi.com/?s=${query}&apikey=${OMDB_API_KEY}`
         );
         const data = await res.json();
-        // console.log(data.Search);
         return data;
     } catch (err) {
-        // console.log(err);
         return err;
     } 
+}
+
+export async function fetchMovieDetail(selectedId) {
+    try {
+        const res = await fetch(
+            `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${selectedId}`
+        );
+        const data = await res.json();
+        return data;
+    } catch (err) {
+
+    }
 }
